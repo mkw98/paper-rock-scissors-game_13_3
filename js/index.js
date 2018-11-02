da39c2c1a7036f34a31592dd24a5c770d6ab4adc
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-11-02 09:42:45 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,5 @@
 'use strict';
-var paper = document.getElementById('paper');
-var scissors = document.getElementById('scissors');
-var rock = document.getElementById('rock');
-var output = document.getElementById('output');
-var result = document.getElementById('result');
-var rounds = document.getElementById('rounds');
-var yourScore = document.getElementById('yourScore');
-var hisScore = document.getElementById('hisScore');
-var newGame = document.getElementById('newGame');
-var playerScore;
-var computerScore;
-var computerChoice;
-var roundsNumber;
+
 //Dla uporządkowania kodu, stwórz zmienną params zawierającą obiekt. 
 //W tym obiekcie przechowuj wszystkie wartości, 
 //które obecnie są zapisane w zmiennych globalnych, 
@@ -21,7 +9,28 @@ var roundsNumber;
 //Dla przykładu, jeśli do tej pory zmienna w Twoim kodzie nazywała się roundsPlayed, 
 //teraz będzie się nazywać params.roundsPlayed.
 
-var params;
+var params = {
+  var paper = document.getElementById('paper');
+  var scissors = document.getElementById('scissors');
+  var rock = document.getElementById('rock');
+  var output = document.getElementById('output');
+  var result = document.getElementById('result');
+  var rounds = document.getElementById('rounds');
+  var yourScore = document.getElementById('yourScore');
+  var hisScore = document.getElementById('hisScore');
+  var newGame = document.getElementById('newGame');
+  var playerScore;
+  var computerScore;
+  var computerChoice;
+  var roundsNumber;
+  
+  
+  //playerWins: 0,
+  //computerWins: 0,
+  //maxRounds: 0,
+  //completeRounds: 0,
+  //progress: [],
+};
                       
 function computerChoiceNumber() {
   var computerChoiceNumber = Math.floor(Math.random() * 3 + 1);
@@ -34,50 +43,49 @@ function computerChoiceNumber() {
   }
 };
 
-
 function playerMove(playerChoice) {
 
   computerChoiceNumber();
-  output.innerHTML = 'Wybrałeś '+playerChoice+'. Komputer wybrał '+computerChoice+'.';
+  params.output.innerHTML = 'Wybrałeś '+playerChoice+'. Komputer wybrał '+computerChoice+'.';
   
   if (playerChoice == 'papier' && computerChoice == 'kamień' ||
       playerChoice == 'nożyce' && computerChoice == 'papier' ||
       playerChoice == 'kamień' && computerChoice == 'nożyce'
   ) {
-    result.innerHTML = 'WYGRYWASZ';
-    playerScore++;
-    yourScore.innerHTML = 'Twój wynik: '+playerScore+'';
-    roundsNumber--;
+    params.result.innerHTML = 'WYGRYWASZ';
+    params.playerScore++;
+    params.yourScore.innerHTML = 'Twój wynik: '+playerScore+'';
+    params.roundsNumber--;
      } else if (playerChoice == 'papier' && computerChoice == 'nożyce' ||
       playerChoice == 'nożyce' && computerChoice == 'kamień' ||
       playerChoice == 'kamień' && computerChoice == 'papier') {
     
-    result.innerHTML = 'PRZEGRYWASZ';
-    computerScore++;
-    hisScore.innerHTML = 'Wynik komputera: '+computerScore+'';
-    roundsNumber--;
+    params.result.innerHTML = 'PRZEGRYWASZ';
+    params.computerScore++;
+    params.hisScore.innerHTML = 'Wynik komputera: '+computerScore+'';
+    params.roundsNumber--;
        
   } else {
-    result.innerHTML = 'REMIS';  
-    roundsNumber--;
+    params.result.innerHTML = 'REMIS';  
+    params.roundsNumber--;
   }
 
   if (roundsNumber == 0) 
     {if (computerScore > playerScore) {
-        rounds.innerHTML = 'GAME OVER' +'<br>' + 'KOMPUTER WYGRAŁ';
+        params.rounds.innerHTML = 'GAME OVER' +'<br>' + 'KOMPUTER WYGRAŁ';
         buttons.classList.add("hide");
         rounds.classList.add("important");
     } else if (playerScore > computerScore) {
-       rounds.innerHTML = 'GAME OVER' + '<br>' + 'KOMPUTER PRZEGRAŁ';
+       params.rounds.innerHTML = 'GAME OVER' + '<br>' + 'KOMPUTER PRZEGRAŁ';
        buttons.classList.add("hide");
        rounds.classList.add("important");
     } else if (playerScore === computerScore) {
-      rounds.innerHTML = 'GAME OVER' + '<br>' + 'REMIS';
+      params.rounds.innerHTML = 'GAME OVER' + '<br>' + 'REMIS';
       buttons.classList.add("hide");
       rounds.classList.add("important");
     }
     } else {
-    rounds.innerHTML = 'Liczba rund do końca gry: '+roundsNumber+'';
+    params.rounds.innerHTML = 'Liczba rund do końca gry: '+roundsNumber+'';
     };
     };
 
@@ -102,24 +110,24 @@ scissors.addEventListener('click', function() {
 
 newGame.addEventListener("click", function() {
    
-  result.innerHTML = " ";
-  output.innerHTML = " ";
-  yourScore.innerHTML = " Twój wynik: 0.";
-  hisScore.innerHTML = " Wynik komputera: 0.";
-  playerScore = 0;
-  computerScore = 0;
-  roundsNumber = window.prompt(
+  params.result.innerHTML = " ";
+  params.output.innerHTML = " ";
+  params.yourScore.innerHTML = " Twój wynik: 0.";
+  params.hisScore.innerHTML = " Wynik komputera: 0.";
+  params.playerScore = 0;
+  params.computerScore = 0;
+  params.roundsNumber = window.prompt(
     "Ile rund chcesz rozegrać?");
   if (roundsNumber === null) {
-    rounds.innerHTML = 'Kliknięto "anuluj". Rezygnujesz? Jeśli nie, kliknij "Nowa gra", aby ustalić, ile razy chcesz zagrać';
+    params.rounds.innerHTML = 'Kliknięto "anuluj". Rezygnujesz? Jeśli nie, kliknij "Nowa gra", aby ustalić, ile razy chcesz zagrać';
   } else if (isNaN(roundsNumber)) {
-    rounds.innerHTML = 'Ile rund chcesz rozegrać? Wpisz tylko cyfry' + '<br><br>';
+    params.rounds.innerHTML = 'Ile rund chcesz rozegrać? Wpisz tylko cyfry' + '<br><br>';
   } else if (roundsNumber === '') {
     rounds.innerHTML = 'Nic nie wpisano. Ile rund chcesz rozegrać?';
   } else if (roundsNumber < 1) {
-    rounds.innerHTML = 'Wpisz liczbę większą od zera.';
+    params.rounds.innerHTML = 'Wpisz liczbę większą od zera.';
   } else {
-    rounds.innerHTML = 'Liczba rund: '+roundsNumber+'';
+    params.rounds.innerHTML = 'Liczba rund: '+roundsNumber+'';
     buttons.classList.remove("hide");
     rounds.classList.remove("important");
   };
